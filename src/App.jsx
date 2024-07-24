6237e09c83aa642152059d0e99a2bcc871520210
user.email <99192547+not-iron-yury@users.noreply.github.com>
2024-07-24 17:49:51 +0300

    Получение данных с бэка (fetch)
    Changes to be committed:
    	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import React from 'react';
+import './scss/app.scss';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
 import Categories from './components/UI/Categories';
 import Sort from './components/UI/Sort';
-import './scss/app.scss';
-import DataPizza from './assets/JSON/pizza.json';
+//import DataPizza from './assets/JSON/pizza.json';
 
 function App() {
+  const [pizzaList, setPizzaList] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://66a0dd137053166bcabd2744.mockapi.io/items')
+      .then((result) => result.json())
+      .then((data) => setPizzaList(data));
+  }, [pizzaList]);
+
   return (
     <>
       <div className='wrapper'>
@@ -18,7 +27,7 @@ function App() {
             </div>
             <h2 className='content__title'>Все пиццы</h2>
             <ul className='content__items'>
-              {DataPizza.map((itm) => (
+              {pizzaList.map((itm) => (
                 <PizzaBlock key={itm.id} pizza={itm} />
               ))}
             </ul>
