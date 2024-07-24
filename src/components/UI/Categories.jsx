cfb4c3c9eae40d23b2594617fe7e606b818121a3
user.email <99192547+not-iron-yury@users.noreply.github.com>
2024-07-24 09:25:55 +0300

    Maping кнопок фильтра товаров
    Changes to be committed:
    	modified:   src/components/Header.jsx
    	modified:   src/components/UI/Categories.jsx

--- src/components/UI/Categories.jsx
+++ src/components/UI/Categories.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
+
 export default function Categories() {
+  const [activeCategory, setActiveCategory] = useState(0);
+  const categories = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  function changeCategory(i) {
+    setActiveCategory(i);
+  }
+
   return (
     <div className='categories'>
       <ul>
-        <li className='active'>Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((itm, i) => (
+          <li
+            key={itm}
+            className={i === activeCategory ? 'active' : ''}
+            onClick={() => changeCategory(i)}>
+            {itm}
+          </li>
+        ))}
       </ul>
     </div>
   );
